3de594c398edbc8c6a451b7296751607e641921c
Damir Pavlik <damir.pavlik@gmail.com>
2023-12-13 22:36:42 +0100

    added arrow down/up logic

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,47 +1,30 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
+import SearchItems from "./SearchItems";
+import { FormProps, SearchData } from "../types";
 
-interface SearchData {
-  id: number;
-  name: string;
-  region: string;
-  country: string;
-  lat: number;
-  lon: number;
-  url: string;
-}
-
-type FormData = {
-  city: string;
-  redirectToPage: (e: React.FormEvent<HTMLFormElement>) => void;
-  handleChange: (value: string) => void;
-  searchData: SearchData[];
-  handleSearchClick: (city: SearchData) => void;
-};
-
-const Form: React.FC<FormData> = ({
+const Form: React.FC<FormProps> = ({
   city,
   redirectToPage,
   handleChange,
   searchData,
   handleSearchClick,
+  rounded,
+  handleKeyPress,
+  idx,
 }) => {
-  const [rounded, setRounded] = useState<boolean>(true);
-
-  useEffect(() => {
-    setRounded(searchData.length > 0 ? false : true);
-  }, [searchData]);
-
   return (
     <Fragment>
       <form onSubmit={(e) => redirectToPage(e)}>
         <div className="flex">
           <input
+            autoComplete="off"
             className={`w-[88%] text-[16px] drop-shadow-md focus:outline-none bg-[#eaecef] py-3 pl-5 ${
               rounded ? "rounded-[30px]" : "rounded-t-[30px]"
             }`}
             value={city}
             name="city"
             onChange={(e) => handleChange(e.currentTarget.value)}
+            onKeyDown={(e) => handleKeyPress(e)}
             placeholder="Enter a City or Country"
           />
           <button className="w-[11.5%] drop-shadow-md ml-auto block bg-[#eaecef] rounded-md">
@@ -49,14 +32,13 @@ const Form: React.FC<FormData> = ({
           </button>
         </div>
         <div className="">
-          {searchData?.map((item: SearchData) => (
-            <div
+          {searchData?.map((item: SearchData, index: number) => (
+            <SearchItems
               key={item.id}
+              item={item}
               onClick={() => handleSearchClick(item)}
-              className="bg-[#eaecef] hover:bg-[#dcdfe3] transition-all duration-300 ease-linear px-5 py-5 w-[88%] cursor-pointer"
-            >
-              {`${item.name} - ${item.country}`}
-            </div>
+              isSelected={idx === index}
+            />
           ))}
         </div>
       </form>
--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -2,15 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 import Form from "./Form";
 
-interface SearchData {
-  id: number;
-  name: string;
-  region: string;
-  country: string;
-  lat: number;
-  lon: number;
-  url: string;
-}
+import { SearchData } from "../types";
 
 const MainCard = () => {
   const [city, setCity] = useState<string>("");
@@ -20,10 +12,16 @@ const MainCard = () => {
   });
 
   const [searchData, setSearchData] = useState<SearchData[]>([]);
+  const [rounded, setRounded] = useState<boolean>(true);
+  const [idx, setIdx] = useState<number | null>(null);
 
   useEffect(() => {
     localStorage.setItem("history", JSON.stringify(history));
   }, [history]);
+
+  useEffect(() => {
+    setRounded(searchData.length > 0 ? false : true);
+  }, [searchData]);
 
   const handleChange = async (value: string) => {
     setCity(value);
@@ -31,7 +29,6 @@ const MainCard = () => {
 
     try {
       let response = await axios.get(apiUrl);
-      console.log(response.data);
       setSearchData(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -51,6 +48,20 @@ const MainCard = () => {
     window.location.href = `/place/${value.url}`;
   };
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowDown") {
+      setIdx((prevIdx) =>
+        prevIdx === null ? 0 : Math.min(prevIdx + 1, searchData.length - 1)
+      );
+      console.log(idx);
+    } else if (e.key === "ArrowUp") {
+      setIdx((prevIdx) => (prevIdx === null ? 0 : Math.max(prevIdx - 1, 0)));
+    } else if (e.key === "Enter" && idx !== null) {
+      e.preventDefault();
+      handleSearchClick(searchData[idx]);
+    }
+  };
+
   return (
     <Fragment>
       <Form
@@ -59,6 +70,9 @@ const MainCard = () => {
         redirectToPage={redirectToPage}
         searchData={searchData}
         handleSearchClick={handleSearchClick}
+        rounded={rounded}
+        handleKeyPress={handleKeyPress}
+        idx={idx}
       />
     </Fragment>
   );
