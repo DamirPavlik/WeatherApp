7b9f80a287f251464c6a715cdb52e73094d9074d
Damir Pavlik <damir.pavlik@gmail.com>
2023-12-13 19:12:43 +0100

    refactoring code in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Fragment } from "react";
+import SideBar from "./components/SideBar";
+import { Home, Place, History } from "./pages";
 
 const App = () => {
   return (
-    <>
-      <Layout />
-    </>
+    <Fragment>
+      <div className="grid grid-cols-12 gap-4 p-[30px] h-screen">
+        <div className="col-span-1">
+          <SideBar />
+        </div>
+        <div className="col-span-11">
+          <BrowserRouter>
+            <Routes>
+              <Route index path="/" element={<Home />} />
+              <Route path="/place/:slug" element={<Place />} />
+              <Route path="/history" element={<History />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
