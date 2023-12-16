179b5ca0d0ac74d23a679acc4b7a2a17bf850a5c
Damir Pavlik <damir.pavlik@gmail.com>
2023-12-16 23:21:08 +0100

    displaying data for daily forecast, adding scrolling into view for current time

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -1,14 +1,49 @@
-import React from "react";
-import { WeatherInfo } from "../types";
+import React, { Fragment, useEffect, useRef } from "react";
 
 const HourlyForecast: React.FC<any> = ({ hourlyData }) => {
-  console.log(hourlyData);
+  const curTimeRef = useRef<HTMLDivElement | null>(null);
+
+  let curTime = new Date();
+  curTime.setHours(curTime.getHours() + 1);
+  curTime.setMinutes(0);
+
+  let roundedTime = curTime.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  useEffect(() => {
+    if (curTimeRef.current) {
+      curTimeRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return (
-    <>
-      {hourlyData.map((hour: any) => (
-        <div>{hour.time}</div>
-      ))}
-    </>
+    <Fragment>
+      <div className="flex overflow-x-auto">
+        {hourlyData.map((hour: any) => {
+          const formattedTime = new Date(hour.time).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+          const isCurTime = formattedTime === roundedTime;
+          return (
+            <div
+              className={`px-[30px] whitespace-nowrap ${formattedTime} ${
+                isCurTime ? "bg-red-400" : ""
+              }`}
+              key={hour.time}
+              ref={isCurTime ? curTimeRef : null}
+            >
+              <p>{formattedTime}</p>
+              <img src={hour.condition.icon} alt="" />
+              <p>{hour.condition.text}</p>
+              <h5>{hour.temp_c}</h5>
+            </div>
+          );
+        })}
+      </div>
+    </Fragment>
   );
 };
 
--- src/components/Place.tsx
+++ src/components/Place.tsx
@@ -4,26 +4,26 @@ import HourlyForecast from "./HourlyForecast";
 import { PlaceProps } from "../types";
 
 const Place = ({ weatherData, error }: PlaceProps) => {
-  const curDay = new Date().toISOString().split("T")[0];
-  const findDay = weatherData?.forecast.forecastday.find(
-    (day) => day.date === curDay
-  );
-  const hourlyData = findDay?.hour.filter((_, i) => i % 3 === 2);
-
   return (
     <div>
       {weatherData ? (
-        <div className="grid grid-cols-12 content-center gap-4 p-[30px]">
-          <div className="col-span-7">
-            <CurrentWeather
-              location={weatherData.location}
-              condition={weatherData.current.condition}
-              temp_c={weatherData.current.temp_c}
-              icon={weatherData.current.condition.icon}
-            />
-            <HourlyForecast hourlyData={hourlyData} />
+        <div>
+          <div className="grid grid-cols-12 content-center gap-4 p-[30px]  mb-[200px]">
+            <div className="col-span-7">
+              <CurrentWeather
+                location={weatherData.location}
+                condition={weatherData.current.condition}
+                temp_c={weatherData.current.temp_c}
+                icon={weatherData.current.condition.icon}
+              />
+            </div>
+            <div className="col-span-5">
+              <DailyForecast dailyData={weatherData.forecast.forecastday} />
+            </div>
           </div>
-          <DailyForecast dailyData={weatherData.forecast.forecastday} />
+          <HourlyForecast
+            hourlyData={weatherData?.forecast.forecastday[0].hour}
+          />
         </div>
       ) : (
         <p>{error}</p>
